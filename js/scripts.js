2daaa4f8af6e17c7dfb7e38c9ef8ff7d466cbf1c
Charles Harvey <harvey.charles@gmail.com>
2021-04-21 16:28:35 +0100

    overflow hidden while loading

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,18 @@
 
         'use strict';
 
+        var $body = $('body');
+
+        $body.addClass('overflow_hidden');
+        $body.addClass('loading');
 
         //MASONRY GALLERY
         putImagesRandomPosition();
         setTimeout(doMasonry, 500);
-        setTimeout(redoMasonry, 1000);
+
         setTimeout(makeImagesSlideIn, 1500);
 
 
-        let $grid;
 
         function putImagesRandomPosition() {
 
@@ -32,25 +35,25 @@
 
 
         function doMasonry() {
-            $grid = $('.grid').masonry({
+            $('.grid').masonry({
                 // options
                 itemSelector: '.grid-item',
                 columnWidth: 20
             });
 
         }
-        function redoMasonry(grid) {
-            console.log('redo masonry'), grid;
-            grid.masonry('layout');
-        }
+
 
         function makeImagesSlideIn() {
             $('.grid-item').removeClass('invisible');
             $('.grid-item').addClass('visible');
+
+            $body.removeClass('overflow_hidden');
+            $body.removeClass('loading');
         }
 
         $(window).on('resize', function (e) {
-            redoMasonry($grid);
+            doMasonry();
         })
 
 
