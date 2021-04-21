2b9e322029209a7bf78a40863fb076a02760cf0a
Charles Harvey <harvey.charles@gmail.com>
2021-04-21 15:51:18 +0100

    add random translate to all grid

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,17 @@
         //MASONRY GALLERY
         doMasonry()
 
+        $('.grid').each(function (i) {
+            $this = $(this);
+            randomX = (Math.random() - 0.5) * 2000;
+            randomY = (Math.random() - 0.5) * 2000;
+            console.log({ randomX, randomY });
+            $this.css({
+                "-webkit-transform": "translate(" + randomX + "px," + randomY + "px)",
+                "transform": "translate(" + randomX + "px," + randomY + "px)",
+            });
+        });
+
 
         function doMasonry() {
             $('.grid').masonry({
